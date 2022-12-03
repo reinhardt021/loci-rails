905010408776ceb063e701d2673684cf55e1bbc8
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-03 18:25:00 +1300

    more cleanup to make the init() more readable

--- app/javascript/loci.js
+++ app/javascript/loci.js
@@ -1,8 +1,6 @@
 import $ from 'jquery'
 import * as THREE from 'three'
-// should include requestAnimationFrame()
 import { TrackballControls } from 'three/addons/controls/TrackballControls.js'
-//import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js'
 
 function setupCamera() {
     // set scene size
@@ -62,6 +60,33 @@ function addSphere(scene, vector, color) {
     scene.add(sphere);
 }
 
+function addItems(scene) {
+    const sphere = addSphere(scene, {x:100, y:100, z:100}, 0xCC0000);
+    const cube = addCube(scene, {x:-100,y:-100,z:-100}, 0x00FF00, {l:50,w:50,h:50});
+    const cubeX = addCube(scene, {x:-200,y:-200,z:-200}, 0x00FF00, {l:50,w:50,h:50});
+
+    const cubeTR = addCube(scene, {x:500,z:500,y:0}, 0x0000FF, {l:50,w:50,h:50});
+    const cubeBR = addCube(scene, {x:500,z:-500,y:0}, 0x0000FF, {l:50,w:50,h:50});
+    const cubeBL = addCube(scene, {x:-500,z:-500,y:0}, 0x0000FF, {l:50,w:50,h:50});
+    const cubeTL = addCube(scene, {x:-500,z:500,y:0}, 0x0000FF, {l:50,w:50,h:50});
+
+}
+
+function addLights(scene) {
+    // light from the left of initial camera
+    //const pointLight = new THREE.PointLight(0xFFFF00);
+    //pointLight.position.set(-7000, 7000, 7000);
+    //scene.add(pointLight);
+
+    // light from above of initial camera
+     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+     directionalLight.position.set(0, 1, 0);
+     scene.add(directionalLight);
+
+    // some light on bottom of initial camera
+    const ambientLight = new THREE.AmbientLight(0x222222);
+    scene.add(ambientLight);
+}
 
 function addSkybox(scene) {
     // skybox
@@ -99,44 +124,7 @@ function createRenderer() {
     return renderer;
 }
 
-function init() {
-    console.log("testing loci init")
-
-    const camera = setupCamera();
-    const scene = new THREE.Scene(); // 3D world
-
-    const sphere = addSphere(scene, {x:100, y:100, z:100}, 0xCC0000);
-    const cube = addCube(scene, {x:-100,y:-100,z:-100}, 0x00FF00, {l:50,w:50,h:50});
-    const cubeX = addCube(scene, {x:-200,y:-200,z:-200}, 0x00FF00, {l:50,w:50,h:50});
-
-    const cubeTR = addCube(scene, {x:500,z:500,y:0}, 0x0000FF, {l:50,w:50,h:50});
-    const cubeBR = addCube(scene, {x:500,z:-500,y:0}, 0x0000FF, {l:50,w:50,h:50});
-    const cubeBL = addCube(scene, {x:-500,z:-500,y:0}, 0x0000FF, {l:50,w:50,h:50});
-    const cubeTL = addCube(scene, {x:-500,z:500,y:0}, 0x0000FF, {l:50,w:50,h:50});
-
-
-    // light from the left of initial camera
-    //const pointLight = new THREE.PointLight(0xFFFF00);
-    //pointLight.position.set(-7000, 7000, 7000);
-    //scene.add(pointLight);
-
-    // light from above of initial camera
-     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-     directionalLight.position.set(0, 1, 0);
-     scene.add(directionalLight);
-
-    // some light on bottom of initial camera
-    const ambientLight = new THREE.AmbientLight(0x222222);
-    scene.add(ambientLight);
-
-
-    addSkybox(scene);
-    addGrid(scene);
-    const renderer = createRenderer();
-
-    const container = $('.container');
-    container.append(renderer.domElement);
-
+function handleWindowResize(scene, camera, renderer) {
     const onWindowResize = () => {
         const WIDTH = window.innerWidth; // var WIDTH = 400;
         const HEIGHT = window.innerHeight; // var HEIGHT = 300;
@@ -146,23 +134,55 @@ function init() {
         //controls.handleResize();
         renderer.render(scene, camera);
     }
-    window.addEventListener('resize', onWindowResize, false);      
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function handleControlUpdates(scene, camera, renderer) {
     const controls = new TrackballControls(camera, renderer.domElement);
     controls.rotateSpeed = 1.0;
-    controls.zoomSpeed = 1.2;
+    controls.zoomSpeed = 5.2;
     controls.panSpeed = 0.8;
     controls.noZoom = false;
     controls.noPan = false;
     controls.staticMoving = true;
-    controls.dynamicDampingFactor = 0.3;
+    //controls.dynamicDampingFactor = 0.3; // only needed if staticMoving = false
     controls.keys = [65, 83, 68];
-    const onControlInputs = () => {
-        renderer.render(scene, camera)
-    }
+    const onControlInputs = () => renderer.render(scene, camera)
     controls.addEventListener('change', onControlInputs);
+
+    return controls;
+}
+
+function init() {
+    console.log("Welcome to Loci the digital sky!")
+
+    const camera = setupCamera();
+    const scene = new THREE.Scene(); // 3D world
+
+    addSkybox(scene);
+    addGrid(scene);
+    addItems(scene);
+    addLights(scene);
+
+    const renderer = createRenderer();
+
+    // note this needs to be before handleControlUpdates()
+    $('.container').append(renderer.domElement);
+
+    handleWindowResize(scene, camera, renderer);
+    const controls = handleControlUpdates(scene, camera, renderer);
 
     renderer.render(scene, camera); // render the scene
 
+    return controls;
 }
 
-init()
+function animate(controls) {
+    // recursion needed to animate the page on control updates
+    window.requestAnimationFrame(() => animate(controls));
+    controls.update();
+}
+
+
+const controls = init();
+animate(controls);
