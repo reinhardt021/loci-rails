9d6cbedfc24193f5f1ee54e8b4ecae8ecf19fe8e
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-03 22:22:17 +1300

    I think I finally have the assets loading as wanted now

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Loci
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif) # DOESNT DO ANYTHING
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,10 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "loci.js.erb"
+pin "loci-2" # this gets rid of the loci not found issue
+#pin "loci" # this gets rid of the loci not found issue
+#pin "loci", to: "./loci.js.erb"
+#pin "loci.js.erb", to: "./loci.js.erb"
 pin "jquery" # @3.6.1
 #pin "three" # @0.147.0
 pin "three", to: "https://unpkg.com/three@0.147.0/build/three.module.js"
